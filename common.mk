b5e60397c06c9063d42e72f37ba9afc7376fcaa1
ppanzenboeck <philipp.panzenboeck1@gmail.com>
2024-06-24 14:44:15 +0200

    use pico gapps config again

--- common.mk
+++ common.mk
@@ -9,6 +9,7 @@ DERP_BUILDTYPE=Official
 DERP_VERSION_APPEND_TIME_OF_DAY=true
 EXTRA_UDFPS_ICONS=true
 TARGET_NOT_USES_BLUR=true
+TARGET_USES_PICO_GAPPS=true
 
 #Viper4Android
 $(call inherit-product, vendor/ViPER4AndroidFXRepackaged/config.mk)
